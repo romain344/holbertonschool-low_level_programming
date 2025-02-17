ededc6a6720b95e6b4e64a8761f70f2f7b45c053
romain344 <romainleoulzec@gmail.com>
2025-02-17 12:03:11 +0100

    génere une chaine de caractére

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-puts("Programming is like building a multilingual puzzle"); return 0; }
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              using the puts fuctiont.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
