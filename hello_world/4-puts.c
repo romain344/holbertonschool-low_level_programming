f8475d2f51e70364e74e4df027e46c48133269f4
romain344 <romainleoulzec@gmail.com>
2025-02-17 12:08:42 +0100

    génere un code caractér

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,5 +8,5 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
