770bafaae20ef511257dc990f7cf24d16950db7c
romain344 <romainleoulzec@gmail.com>
2025-02-17 16:14:25 +0100

    sort des caractére en chaine

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
