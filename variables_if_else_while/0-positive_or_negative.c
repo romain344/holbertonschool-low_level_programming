416273bc03137f90a4c04b4055e0f493cce783c6
romain344 <romainleoulzec@gmail.com>
2025-02-18 10:48:20 +0100

    génere un nonbre aleatoire

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));		/* Initialisation du générateur de nombres aléatoires */
+	n = rand();			/* Générer un nombre aléatoire */
+
+	printf("%d ", n);	/* Afficher le nombre généré */
+
+	if (n > 0)
+	{
+		printf("is positive\n");	/* Si le nombre est positif */
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");		/* Si le nombre est égal à 0 */
+	}
+	else
+	{
+		printf("is negative\n");	/* Si le nombre est négatif */
+	}
+
+	return (0);		/* Retourner 0 pour indiquer le succès */
+}
