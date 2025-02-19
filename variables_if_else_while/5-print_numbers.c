be8c21911bc199920c3164af2b148dbae279f318
romain344 <romainleoulzec@gmail.com>
2025-02-19 18:18:36 +0100

    print all in number 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point oh the program
+ *
+ * Description:writtet all number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int b = 0;
+
+	while (b < 10)
+	{
+		prinft("%d", b);
+		b++;
+	}
+	printf("\n");
+	return (0);
+}
