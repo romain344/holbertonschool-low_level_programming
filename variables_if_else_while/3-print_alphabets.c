36c897c8ab104ceda619cf3ae1cec13c28691db7
romain344 <romainleoulzec@gmail.com>
2025-02-19 15:04:01 +0100

    written in lower case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: written that prints in lowercase and in uppercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(lettre)
+		return (0);
+}
