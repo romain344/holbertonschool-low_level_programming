d8df93cbc2801069007fbe5614413a8e57d458d6
romain344 <romainleoulzec@gmail.com>
2025-02-19 11:09:45 +0100

    generate number random

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: generate a random number
+ * Description: generate a ramdom nuber
  *
  * Return 0 (success)
  */
@@ -16,11 +16,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
-		printf("%d and is greater than 5\n", n);
-	esle if (n > 5)
-		prinft("%d and is 0\n", n);
-	if (n > 6)
-		printf("%d and is less than 6 and not 0\n, n);
+	last_digit = abs(n % 10);
+
+        printf("last digit of %d is %d ", n, last_digit);
+
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
