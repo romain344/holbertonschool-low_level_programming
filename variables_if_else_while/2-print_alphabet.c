dc7ee8dc9502d2f8c9da42f1b4b2355867690867
romain344 <romainleoulzec@gmail.com>
2025-02-19 11:41:08 +0100

    wriyyen in lower case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point fo the program
+ *
+ * Descrtion: written il lower case
+ *
+ * Retrun: Always à (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	while (a <= 'z')
+	{
+		putchar(a);
+	}
+
+	putchar('n\n');
+
+	return (0);
+}
+
