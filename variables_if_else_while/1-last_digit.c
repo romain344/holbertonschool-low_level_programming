c499aa01ccb600766a2c6492e1f8f5182bfe53aa
romain344 <romainleoulzec@gmail.com>
2025-02-19 10:38:45 +0100

    generate ramdom number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: generate a random number
+ *
+ * Return 0 (success)
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n > 5)
+		printf("%d and is greater than 5\n", n);
+	esle if (n > 5)
+		prinft("%d and is 0\n", n);
+	if (n > 6)
+		printf("%d and is less than 6 and not 0\n, n);
 	return (0);
 }
