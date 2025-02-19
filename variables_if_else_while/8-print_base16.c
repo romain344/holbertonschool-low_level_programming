a96fb8e17f2b3188100176695a4fe30a0cf93289
romain344 <romainleoulzec@gmail.com>
2025-02-19 19:34:39 +0100

    print the nombers in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the programe
+ *
+ * Description: number 16
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int b = 0;
+
+	while (a < 16)
+	{
+		if (a < 10)
+		{
+			putchar (b + '0');
+			a++;
+		}
+		else
+		{
+			putchar(b - 10 + 'a');
+			a++
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
