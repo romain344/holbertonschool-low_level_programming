866f7893f321f445c9d39fc582253e67441f0a9a
romain344 <romainleoulzec@gmail.com>
2025-02-19 16:30:14 +0100

    ...

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: written that prints in lowercase and in uppercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+
+		while (a <= 'z')
+		{
+			if (a == 'e' || a == 'q')
+			{
+				a++;
+				continue;
+			}
+			else
+			{
+				putchar(a)
+				a++;
+			}
+		}
+
+	putchar('\n');
+	return (0);
+}
