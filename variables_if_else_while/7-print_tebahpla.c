ee8bd9553ed20eadec21c794db49d64d16a0b306
romain344 <romainleoulzec@gmail.com>
2025-02-19 19:08:17 +0100

    reverse in alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Descriptiont: alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a = "z";
+
+	while (a >= 'a')
+	{
+		puchar(a)
+		a--;
+	}
+
+	puchar('\n');
+	return (0);
+}
