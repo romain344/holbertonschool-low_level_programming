f2467eeba56cccc709124bc8890fc9b7a3ad58f3
romain344 <romainleoulzec@gmail.com>
2025-02-21 14:38:29 +0100

    function that prints x10 the alphabet int lowercase

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that print x10 alphabet in lowercase
+ *
+ * description:
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char a;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putachar (a);
+		}
+
+		_putchar('\n');
+}
