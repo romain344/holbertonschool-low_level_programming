b5b78efd42b4c8e38e17bd2b9399f63bd831d6d4
romain344 <romainleoulzec@gmail.com>
2025-02-21 13:01:54 +0100

    printf lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase
+ *
+ * Description:
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++)
+	{
+		_putchar(a);
+	}
+
+	putchar('\n');
+}
