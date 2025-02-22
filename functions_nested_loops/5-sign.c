71fa880da693052e6eccff60ffdec74adaf147a7
romain <197885038+romain344@users.noreply.github.com>
2025-02-22 12:34:53 +0000

    print the of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_sing - print print the of a number
+* @n: the number in check
+*
+* Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+*/
+int print_sign(int n )
+{
+
+    if (n > 0)
+    {
+    
+        _putchar('+');
+        return (1);
+    }
+    else if (n== 0)
+    {
+        _putchar('0');
+        return (0)
+    }
+    else
+    {
+        _putchar('-');
+        return
+    }
+}
+    
+    
+    
+    
+    
