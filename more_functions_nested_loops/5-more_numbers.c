86069382cb14ecc3c04030f5f7bb75e2af6c5ff8
romain344 <romainleoulzec@gmail.com>
2025-02-24 15:41:11 +0100

    print a function that write 10x the numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 	char a, b;
 
-	for (a = 0; a <= 14; a++)
+	for (a = 0; a <= 10; a++)
 	{
-		for (b = 0; b <= 10; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
 			{
