95f9f6a257eddce5e8b566206748a105ed4a4634
romain344 <romainleoulzec@gmail.com>
2025-02-24 13:48:44 +0100

    prints the numbers from 0,9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print__numbers - prints the numbers form 0;9
+ * @c
+ *
+ */
+void print_numbers(void);
+{
+	char panzer;
+
+	if (panzer > '0' panzer <= '9')
+		panzer++;
+	_putachar('\n');
+}
