b0d6fd063ff51e54fc2ec882789cf3f59c4bed8e
romain344 <romainleoulzec@gmail.com>
2025-02-24 14:12:42 +0100

    prints tne numbers from 0to9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * print__numbers - prints the numbers form 0;9
- * @c
- *
+ * print_numbers - prints the numbers form 0;9
  */
-void print_numbers(void);
+void print_numbers(void)
 {
-	char panzer;
-
-	if (panzer > '0' panzer <= '9')
-		panzer++;
-	_putachar('\n');
+	for (char panzer = '0'; panzer <= '9'; panzer++)
+	{
+		-putchar(panzer);
+	}
+	_putchar('\n');
 }
