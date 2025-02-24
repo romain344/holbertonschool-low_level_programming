454397d09bddd3955f77b7a80f1e704011a611e4
romain344 <romainleoulzec@gmail.com>
2025-02-24 14:33:53 +0100

    function that print the number form 0 to 9

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - poster yhe numners of 0,9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; i <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
