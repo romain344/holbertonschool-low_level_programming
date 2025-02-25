46fab727c99cec44aab8b48cda6cab8b517a09db
romain344 <romainleoulzec@gmail.com>
2025-02-25 14:37:14 +0100

    prototype

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @a
+ *
+ * Return: the length of the chain
+ */
+int _strlen(char *a)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		a++;
+	}
+
+	return (length);
+}
+
