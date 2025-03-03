01aa57209371c49689f9e4148e1268becbe8327b
romain344 <romainleoulzec@gmail.com>
2025-03-03 13:29:32 +0100

    prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,5 @@ char *cap_string(char *);
 char *leet(char *);
 void print_triangle(int size);
 char *_memset(char *s,char b,unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
