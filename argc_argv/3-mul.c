f1ed102c0029aaf40b7efc188b3d3381b00ade28
romain344 <romainleoulzec@gmail.com>
2025-03-06 18:34:31 +0100

    multiplie

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
-	
+
 	printf("%d\n", result);
 
 	return (0);
