eceabd8858e06078a502949754a1d1f01a410f91
romain344 <romainleoulzec@gmail.com>
2025-03-07 10:10:07 +0100

    task4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -3,38 +3,55 @@
 #include <ctype.h>
 
 /**
- * main - Adds positive numbers.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of strings representing the arguments.
- *
- * Return: 0 if successful, 1 if there are errors.
- */
+* is_number - checks if a string contains only digits
+* @str: the string to check
+*
+* Return: 1 if it' s a valid number, 0 otherwise#include <stdio.h>
+*/
+int is_number(char *str)
+{
+	int i = 0;
+
+	if (str[i] =='\0')
+		return (0);
+
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+
+/**
+* main - add positive numbers passed as arguments
+* @argc: the number of arguments
+* @argv: array containig the arguments as strings
+*
+* Return: 0 (success), 1 (error if an argument is not number)
+*/
 int main(int argc, char *argv[])
 {
-		int sum = 0;
-		int i, j;
+	int i, sum = 0;
 
-		if (argc == 1)
-		{
+	if (argc == 1)
+	{
 		printf("0\n");
-			return (0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
 		}
-			for (i = 1; i < argc; i++)
-			{
+		sum += atoi(argv[i]);
+	}
 
-				for (j = 0; argv[i][j] != '\0'; j++)
-				{
-					if (!isdigit(argv[i][j]))
-					{
-					printf("error\n");
-						return (1);
-					}
-				}
-
-				sum += atoi(argv[i]);
-			}
-
-			printf("%d\n", sum);
-
-			return (0);
+	printf("%d\n", sum);
+	return (0);
 }
