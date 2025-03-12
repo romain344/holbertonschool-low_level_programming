bcf007270df17a44d24d4f659d665f45dc40a5e6
romain344 <romainleoulzec@gmail.com>
2025-03-12 13:50:11 +0100

    tack1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,24 +15,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 	unsigned int len1 = 0, len2 = 0, i, j;
 
-	if (s1 != NULL)
-		len1 = strlen(s1);
-	if (s2 != NULL)
-		len2 = strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
 
-	if (n > len2)
-		n = len2;
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2)
+	n = len2;
+
 	concat = malloc(len1 + n + 1);
 	if (concat == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
+	concat[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-		concat[i + j] = s2[j];
+	concat[i + j] = s2[j];
 
 	concat[i + j] = '\0';
 
 	return (concat);
-}
+	}
