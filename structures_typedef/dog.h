2a529911f336947a40cbff63d25a62a8b1237196
romain344 <romainleoulzec@gmail.com>
2025-03-12 15:43:48 +0100

    define the following elements name age owner

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Le nom du chien
+ * @age: L'âge du chien
+ * @owner: Le nom du propriétaire du chien
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
