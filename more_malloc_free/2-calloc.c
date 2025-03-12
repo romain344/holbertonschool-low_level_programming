d77c26670210eecce4118c2f9a01586968f3687b
romain344 <romainleoulzec@gmail.com>
2025-03-12 13:28:54 +0100

    tack2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,16 +13,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
-
 	if (ptr == NULL)
 		return (NULL);
-	memset(ptr, 0, nmemb * size);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
