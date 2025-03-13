6e45590f34d97fd85512eac5250b5f74ba184f75
romain344 <romainleoulzec@gmail.com>
2025-03-13 16:16:25 +0100

    prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,16 +7,16 @@
  * @age: L'âge du chien
  * @owner: Le nom du propriétaire du chien
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-typedef struct dog dog_t;
+
 #endif
