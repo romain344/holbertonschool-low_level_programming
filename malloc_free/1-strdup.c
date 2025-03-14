82524659b46246ce50b87353d549e20a2f4d113f
romain344 <romainleoulzec@gmail.com>
2025-03-14 11:57:00 +0100

    tack1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,22 +12,23 @@
  */
 char *_strdup(char *str)
 {
-	char *copy;
+	char *dup;
 	unsigned int i, len;
 
 	if (str == NULL)
 		return (NULL);
 
 	for (len = 0; str[len] != '\0'; len++)
+		;
 
-	copy = malloc(sizeof(char) * (len + 1));
-
-	if (copy == NULL)
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
 		return (NULL);
+
 	for (i = 0; i < len; i++)
-		copy[i] = str[i];
+		dup[i] = str[i];
 
-	copy[len] = '\0';
+	dup[len] = '\0';
 
-	return (copy);
+	return (dup);
 }
