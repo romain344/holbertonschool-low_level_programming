6d65a74aadd7540ab66128f106c5ca32931c420d
romain344 <romainleoulzec@gmail.com>
2025-03-14 13:17:24 +0100

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,50 +1,46 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - Crée une nouvelle structure dog_t et alloue de la mémoire
- * pour les chaînes name et owner.
- * @name: nom du chien
- * @age: âge du chien
- * @owner: propriétaire du chien
+ * new_dog - creates a new dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
  *
- * Return: Un pointeur vers une nouvelle structure dog_t, ou NULL si
- * l'allocation échoue.
+ * Return: a pointer to the new dog, or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *new_name, *new_owner;
-	int i, name_len = 0, owner_len = 0;
+	dog_t *new_dog;
+	int name_len, owner_len;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
 
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
 
-	new_name = malloc(sizeof(char) * (name_len + 1));
-	new_owner = malloc(sizeof(char) * (owner_len + 1));
-
-	if (new_name == NULL || new_owner == NULL)
+	name_len = strlen(name);
+	new_dog->name = malloc(name_len + 1);
+	if (new_dog->name == NULL)
 	{
-		free(new_name);
-		free(new_owner);
-		free(d);
+		free(new_dog);
 		return (NULL);
 	}
 
-	for (i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
+	strcpy(new_dog->name, name);
 
-	d->name = new_name;
-	d->age = age;
-	d->owner = new_owner;
+	owner_len = strlen(owner);
+	new_dog->owner = malloc(owner_len + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
+	new_dog->age = age;
 
-	return (d);
+	return (new_dog);
 }
+
