367fc5f7b74da88d13e2646503e21e0d69a9a9c1
romain344 <romainleoulzec@gmail.com>
2025-03-14 11:28:39 +0100

    function that frees a 2 dimensional grid previously created by your alloc_grid function

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 /**
  * str_concat - Concatenates two strings.
  * @s1: The first string.
@@ -13,39 +14,28 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	unsigned int i, j, len1, len2;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
+	if (s1 != NULL)
 	{
-		s1 = "";
+		while (s1[len1] != '\0')
+			len1++;
 	}
-	if (s2 == NULL)
+	if (s2 != NULL)
 	{
-		s2 = "";
+		while (s2[len2] != '\0')
+			len2++;
 	}
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-
-	concat = malloc(sizeof(char) * (len1 + len2 + 1));
-
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0; i < len1; i++)
-	{
-		 concat[i] = s1[i];
-	}
-	for (j = 0; j < len2; j++)
-	{
-		concat[i + j] = s2[j];
-	}
+		concat[i] = s1[i];
 
-	concat[len1 + len2] = '\0';
+	for (j = 0; j < len2; j++, i++)
+		concat[i] = s2[j];
 
+	concat[i] = '\0';
 	return (concat);
 }
