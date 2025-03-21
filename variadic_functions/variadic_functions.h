5e7b33037e73cee7127bda402c08f8acbacfa386
romain344 <romainleoulzec@gmail.com>
2025-03-21 10:17:53 +0100

    prototype

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
