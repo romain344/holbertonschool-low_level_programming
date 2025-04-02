948976b46d78bce484334822a686308c3f0274fa
romain <romainleoulzec@gmail.com>
2025-04-02 22:16:48 +0200

    prototype

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct list_s
+{
+	char str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+#endif
