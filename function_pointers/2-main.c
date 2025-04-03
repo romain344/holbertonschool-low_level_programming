8d2639d111028bb54312d93fc802bbc86a8cfe0a
romain344 <romainleoulzec@gmail.com>
2025-04-03 10:19:59 +0200

    test

--- function_pointers/2-main.c
+++ function_pointers/2-main.c
@@ -1,4 +1,8 @@
-s_98 - check if a number is equal to 98
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
  * @elem: the integer to check
  *
  * Return: 0 if false, something else otherwise.
