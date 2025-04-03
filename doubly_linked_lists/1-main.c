4ced3e602ca26c520dc9fe1c31616efacb389c59
romain344 <romainleoulzec@gmail.com>
2025-04-03 13:27:50 +0200

    exsecute the programe

--- doubly_linked_lists/1-main.c
+++ doubly_linked_lists/1-main.c
@@ -4,31 +4,31 @@
 #include "lists.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
-	    list_t *head;
-	        list_t *new;
-		    list_t hello = {"World", 5, NULL};
-		        size_t n;
+    dlistint_t *head;
+    dlistint_t *new;
+    dlistint_t hello = {8, NULL, NULL};
+    size_t n;
 
-			    head = &hello;
-			        new = malloc(sizeof(list_t));
-				    if (new == NULL)
-					        {
-							        printf("Error\n");
-								        return (1);
-									    }
-				        new->str = strdup("Hello");
-					    new->len = 5;
-					        new->next = head;
-						    head = new;
-						        n = list_len(head);
-							    printf("-> %lu elements\n", n);
-							        free(new->str);
-								    free(new);
-								        return (0);
+    head = &hello;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        dprintf(2, "Error: Can't malloc\n");
+        return (EXIT_FAILURE);
+    }
+    new->n = 9;
+    head->prev = new;
+    new->next = head;
+    new->prev = NULL;
+    head = new;
+    n = dlistint_len(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (EXIT_SUCCESS);
 }
